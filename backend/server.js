ad6392bb8bdc6ea428c0f93956936199b0eebe4b
Menaka <gmenaka25@gmail.com>
2026-03-16 15:21:12 +0530

    add home route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+  res.send("Todo API is running 🚀");
+});
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
